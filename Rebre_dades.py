fbb0169e42b3d04e24ee945c62b3f2f2a76086cb
Nerea Sánchez <nerea.sanchez7740@iesmacardona.cat>
2024-05-30 14:04:42 +0200

    Codi morse i programa rebre dades i programa enviar dades

--- Rebre_dades.py
+++ Rebre_dades.py
@@ -8,43 +8,21 @@ nom_port = "/dev/cu.usbmodem14101"  # Arduino cable
 
 cansat = serial.Serial(nom_port, BAUD_RATE)
 
-while True:
-    lectura = cansat.readline().decode('Ascii').rstrip("\r\n")
-<<<<<<< HEAD
-    print (lectura)
-    funcions_rebre.separar_mode_lectura(lectura)
-    
-=======
-    
-    estat = lectura.split('#') #Separa l'estat del cansat i les dades que ens envia 
-    print(estat)
-    mode = estat[0]
-    if mode==0 :
-        print("El cansat està en repòs")
-    elif mode==1 :
-        print("El cansat està connectat")
-    elif mode==2 : 
-        print("El cansat s'està localitzant")
-    elif mode==3 : 
-        print("dades cansat") #afegir les dades de la lectura
-    elif mode==4:
-        print("") #dades del senor IR
-    elif mode==5:
-        print("") #enviar missatge al cansat
-    else:
-        print("") #envia les dades del geolocaltzador
-    
-#Així podem accedir a cada valor i guardar-lo en una variable
-dada1 = dades[0]
-dada2 = int(dades[1]) # Les dades són un text, per tant, s'han de convertir al tipus que volem
-dada3 = dades[2]
-dada4 = float(dades[3]) 
-dada5 = dades[4]
+comptador_zeros = 0
 
-print(f"Dada 1: {dada1}")
-print(f"Dada 2: {dada2}")
-print(f"Dada 3: {dada3}")
-print(f"Dada 4: {dada4}")
-print(f"Dada 5: {dada5}")
+while True:
+
+    lectura = funcions_rebre.rebre_dades(cansat)
     print(lectura)
->>>>>>> e169d85d0953b280a30887c36695850b819c38e3
+    estat = funcions_rebre.separar_mode(lectura)
+    missatge = funcions_rebre.separar_missatge(lectura)
+
+    if estat == 3:
+        funcions_rebre.analisis_dades(missatge)
+    if estat == 4:
+        if missatge == "0":
+            comptador_zeros += 1
+    # escriure quants de uns són un punt i una retxa
+    #intentar passar de codi morse a lletres
+    else:
+        funcions_rebre.mostrar_missatge(missatge)
