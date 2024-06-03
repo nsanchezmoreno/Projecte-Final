aaf62bba3780ce2ed5d03e7d38c86ee9d5f2d299
nsanchezmoreno <160328763+nsanchezmoreno@users.noreply.github.com>
2024-06-03 11:54:14 +0200

    Update(Afegir comentaris) Rebre_dades.py

--- Rebre_dades.py
+++ Rebre_dades.py
@@ -8,18 +8,18 @@ nom_port = "/dev/cu.usbmodem14101"  # Arduino cable
 
 cansat = serial.Serial(nom_port, BAUD_RATE)
 
-comptador_zeros = 0
+comptador_zeros = 0 #Iniciam un comptador que s'utilitzarà quan el cansat ens envii dades del senor IR. Dins aquest comptador es guardaran els 0 que ens arribin per a després transcriure-ls.
 
 while True:
 
-    lectura = funcions_rebre.rebre_dades(cansat)
-    print(lectura)
-    estat = funcions_rebre.separar_mode(lectura)
-    missatge = funcions_rebre.separar_missatge(lectura)
+    lectura = funcions_rebre.rebre_dades(cansat) #La lectura es determina a partir de la funció de rebre_dades, situada al document de funcions_rebre
+    print(lectura) #Imprimeix la lectura.
+    estat = funcions_rebre.separar_mode(lectura) # L'estat es determina a partir de la funció de separar_mode, situada al document funcins_rebre.
+    missatge = funcions_rebre.separar_missatge(lectura) #El missatge es determina a partir de la funció separar_missatge, situada al document funcions_rebre.
 
-    if estat == 3:
+    if estat == 3: #Quan l'estat és el nombre 3, s'imprimeixen les dades analitzades a la funció analisis_dades, situada al document funcions.rebre.
         funcions_rebre.analisis_dades(missatge)
-    if estat == 4:
+    if estat == 4: #Quan l'estat és el 4, s'analitzen les dades del sensor IR a través del següent programa
         if missatge == "0":
             comptador_zeros += 1
             if len(comptador_zeros) >10: #Si el nombre de zeros és major que 10, hi ha hagut un error
