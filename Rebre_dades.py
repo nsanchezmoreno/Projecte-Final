c3db6ea6e849cbd92978d7ae8589761a1aa9890c
Nerea Sánchez <nerea.sanchez7740@iesmacardona.cat>
2024-06-04 09:09:27 +0200

    Copia

--- Rebre_dades.py
+++ Rebre_dades.py
@@ -8,21 +8,21 @@ nom_port = "/dev/cu.usbmodem14101"  # Arduino cable
 
 cansat = serial.Serial(nom_port, BAUD_RATE)
 
-comptador_zeros = 0 #Iniciam un comptador que s'utilitzarà quan el cansat ens envii dades del senor IR. Dins aquest comptador es guardaran els 0 que ens arribin per a després transcriure-ls.
+comptador_zeros = 0
 
 while True:
 
-    lectura = funcions_rebre.rebre_dades(cansat) #La lectura es determina a partir de la funció de rebre_dades, situada al document de funcions_rebre
-    print(lectura) #Imprimeix la lectura.
-    estat = funcions_rebre.separar_mode(lectura) # L'estat es determina a partir de la funció de separar_mode, situada al document funcins_rebre.
-    missatge = funcions_rebre.separar_missatge(lectura) #El missatge es determina a partir de la funció separar_missatge, situada al document funcions_rebre.
+    lectura = funcions_rebre.rebre_dades(cansat)
+    print(lectura)
+    estat = funcions_rebre.separar_mode(lectura)
+    missatge = funcions_rebre.separar_missatge(lectura)
 
-    if estat == 3: #Quan l'estat és el nombre 3, s'imprimeixen les dades analitzades a la funció analisis_dades, situada al document funcions.rebre.
+    if estat == 3:
         funcions_rebre.analisis_dades(missatge)
-    if estat == 4: #Quan l'estat és el 4, s'analitzen les dades del sensor IR a través del següent programa
+    if estat == 4:
         if missatge == "0":
             comptador_zeros += 1
-            if len(comptador_zeros) >10: #Si el nombre de zeros és major que 10, hi ha hagut un error
+            if len(comptador_zeros) >10: #Si el nombre de zeros és major que 10, fa un bot de línia per a indicar que aquella lletra ja ha acabat
                 print("\n") 
             elif len(comptador_zeros) <= 4: #si el nombre de zeros és més petit o equivalent a 4 significa que ens envien un ·
                 print("·") 
--- funcions_rebre.py
+++ funcions_rebre.py
@@ -1,48 +1,64 @@
-def separar_mode_lectura(lectura):
-    estat = lectura.split("#") [0]
-def separar_dades_lectura(lectura):
+import math
+def separar_mode(lectura):
+    estat = lectura.split("/") [0]
+    return estat
+    
+def separar_missatge(lectura):
     dades = lectura.split("/") [1]
+    return dades
 
-def dades_lectura():
-    dades_separades = dades.split(",") 
-    dada1 = int(dades_separades[0]) #nombre de paquet
-    dada2 = dades_separades[1] #nom del cansat
-    dada3 = int(dades_separades[2]) #temperatura del termistor
-    dada4 = int(dades_separades[3]) #pressió BMP280
-    dada5 = int(dades_separades[4]) #temperatura BMP280
+def crear_array(dades):
+    dades_separades = dades.split(',')
+    return dades_separades
 
-   #afegir més dades si són necessàries
+def num_paquet(dades_separades):
+    num_paquet = int(dades_separades[0]) #nombre de paquet
+    return num_paquet 
 
-    print(f"Dada 1: {dada1}")
-    print(f"Dada 2: {dada2}")
-    print(f"Dada 3: {dada3}")
-    print(f"Dada 4: {dada4}")
-    print(f"Dada 5: {dada5}")
-    print(f"Dada 6: {dada6}")
+def nom_cansat(dades_separades):
+    nom_cansat= dades_separades[1] #nom del cansat
+    return nom_cansat
+    
+def temperatura(dades_separades):
+    temperatura= int(dades_separades[2]) #temperatura del termistor
+    return temperatura
+
+def pressio(dades_separades):
+    pressio= int(dades_separades[3]) #pressió BMP280
+    return pressio
+    
+def temperatura_BMP(dades_separades):
+    temperatura_BMP = int(dades_separades[4]) #temperatura BMP280
+    return temperatura_BMP
     
 
-def codi_morse():
-While len(codi_morse) <4:
-    while True:
-        lectura_0 = cansat.readline().decode('Ascii').rstrip("\r\n")
-        saber_estat = lectura_0.split('/')
-        estat = separar_mode_lectura
-        dades=
-        lectura_IR=dades
-        print(lectura_IR)#No és necessari
-        if estat != 4:
-            break
-        if lectura_IR == "0"
-         ir += lectura_IR
-        
-    if len(ir)>=8:
-        break
-    elif len(ir)<=3:
-        codi_morse += "."
-        print(".")
-        ir=""
-    else:
-        codi_morse += "-"
-        ir= ""
-    return 
-        
+def mostrar_missatge(missatge):
+    print(missatge)
+
+def analisis_dades(missatge):
+    dades_separades=crear_array(missatge)
+    num_paquet = num_paquet(dades_separades)
+    nom_cansat = nom_cansat(dades_separades)
+    temperatura = temperatura(dades_separades)
+    pressio = pressio(dades_separades)
+    temperatura_BMP = temperatura_BMP(dades_separades)
+    #Calcular temperatura a partir de la lectura del termistor
+    r_aux = 10 #kilohms
+
+    vol_ter = (5*temperatura)/1023 #Calcula els volts del termistor
+
+    r_ter = (vol_ter*r_aux)/(5-vol_ter) #Calcula la resistència del termistor a partir dels volts
+
+    t = round(73.74 - 21.06*math.log(r_ter),3) #Calcula la temperatura real en ºC
+
+    #test
+    print(f"Número de paquet: {num_paquet}")
+    print(f"Nom del cansat: {nom_cansat}")
+    print(f"La temperatura del termistor és de {t}ºC")
+    print(f"La pressió és de {pressio} Pa")
+    print(f"La temperatura del BMP és de {temperatura_BMP}ºC")
+
+
+def rebre_dades(cansat):
+    lectura= cansat.readline().decode('Ascii').rstrip("\r\n")
+    return lectura
